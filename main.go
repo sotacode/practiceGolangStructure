d7acbd07f895b51ecf71d9d21136b62a5d98164e
SotaCode <sotacodeprivate@gmail.com>
2023-07-20 18:28:04 -0400

    variables y retorno de variables

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"src/github.com/sotacode/practiceGolangStructure/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	//variables.MuestroEnteros()
+	//variables.RestoVariables()
+	estado, texto := variables.ConviertoATexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
